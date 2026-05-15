49a3bb552707b68e12c6e8f8557237b491866799
Nicolas Canfrere <nicolas.canfrere@gmail.com>
2026-05-15 18:52:42 +0200

    feat(room): add RoomCsvRow DTO, update CSV parser to require number,floor columns

---
+++ src/Room/Application/Service/RoomCsvRow.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Room\Application\Service;
+
+final readonly class RoomCsvRow
+{
+    public function __construct(
+        public string $number,
+        public int $floor,
+    ) {
+    }
+}
