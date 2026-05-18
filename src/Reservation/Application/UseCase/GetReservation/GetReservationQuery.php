031eba9468776cd2ff3ec2a7de580ce47edb171a
Nicolas Canfrere <nicolas.canfrere@gmail.com>
2026-05-18 21:11:58 +0200

    fix(reservation): correct GetReservationQuery generic type to Reservation

--- src/Reservation/Application/UseCase/GetReservation/GetReservationQuery.php
+++ src/Reservation/Application/UseCase/GetReservation/GetReservationQuery.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Reservation\Application\UseCase\GetReservation;
 
+use App\Reservation\Domain\Model\Reservation;
 use App\Shared\Application\Bus\SyncQueryInterface;
 
-/** @implements SyncQueryInterface<?array> */
+/** @implements SyncQueryInterface<?Reservation> */
 final readonly class GetReservationQuery implements SyncQueryInterface
 {
     public function __construct(public string $id)
