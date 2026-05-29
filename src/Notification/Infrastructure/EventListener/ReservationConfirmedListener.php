d6de5625a368000d97f9cc210c3c8496b0fd3089
Nicolas Canfrere <nicolas.canfrere@gmail.com>
2026-05-29 19:33:40 +0200

    refactor(notification): use Shared integration events in event listener

--- src/Notification/Infrastructure/EventListener/ReservationConfirmedListener.php
+++ src/Notification/Infrastructure/EventListener/ReservationConfirmedListener.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Notification\Infrastructure\EventListener;
 
 use App\Notification\Application\UseCase\SendBookingConfirmationEmail\SendBookingConfirmationEmailCommand;
-use App\Reservation\Domain\Event\ReservationConfirmed;
 use App\Shared\Application\Bus\AsyncCommandDispatcherInterface;
+use App\Shared\Domain\Event\ReservationConfirmed;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 #[AsEventListener(event: ReservationConfirmed::class)]
