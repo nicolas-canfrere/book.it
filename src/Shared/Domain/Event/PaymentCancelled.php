085a32cdc5b868d905b181eb407e8d38a96a7ca1
Nicolas Canfrere <nicolas.canfrere@gmail.com>
2026-06-05 01:37:55 +0200

    feat(payment): add PaymentConfirmed and PaymentCancelled domain events

---
+++ src/Shared/Domain/Event/PaymentCancelled.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Domain\Event;
+
+final readonly class PaymentCancelled
+{
+    public function __construct(
+        public string $reservationId,
+    ) {
+    }
+}
