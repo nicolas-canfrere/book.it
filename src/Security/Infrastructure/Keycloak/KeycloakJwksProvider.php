d99898592b4e16e73a8d6adb108dc189a5d6f95d
Nicolas Canfrere <nicolas.canfrere@gmail.com>
2026-06-07 12:09:07 +0200

    style(security): remove unused Firebase\JWT\Key import from stub

--- src/Security/Infrastructure/Keycloak/KeycloakJwksProvider.php
+++ src/Security/Infrastructure/Keycloak/KeycloakJwksProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Security\Infrastructure\Keycloak;
 
-use Firebase\JWT\Key;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
