f2f3d03d50fed8dd7eeb99aabbbabd9cc5d86afe
Nicolas Canfrere <nicolas.canfrere@gmail.com>
2026-06-07 12:08:37 +0200

    test(security): add KeycloakJwksProvider integration tests

---
+++ src/Security/Infrastructure/Keycloak/KeycloakJwksProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Infrastructure\Keycloak;
+
+use Firebase\JWT\Key;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+final class KeycloakJwksProvider implements KeycloakJwksProviderInterface
+{
+    public function __construct(
+        private readonly HttpClientInterface $httpClient,
+        private readonly CacheInterface $cache,
+        private readonly string $keycloakBaseUrl,
+        private readonly string $keycloakRealm,
+    ) {
+    }
+
+    public function getPublicKeys(): array
+    {
+        throw new \RuntimeException('Not implemented');
+    }
+}
